55047eac9ccd26281946fa49c5707445b43f458d
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2024-12-28 20:14:22 -0300

    exercise 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,50 @@ const Button = ({ onClick, text }) => {
 }
 
 const Stat = ({ stat, amount }) => {
+  if (!amount || amount === 0) {
+    return (
+      <p>{stat}: 0</p>
+    )
+  }
   return (
     <p>{stat}: {amount}</p>
   )
 }
 
+const Percent = ({ text, amount }) => {
+  if (!amount || amount === 0) {
+    return (
+      <p>{text}: 0 %</p>
+    )
+  }
+  return (
+    <p>{text}: {amount} %</p>
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handlerClickGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handlerClickNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
   const handlerClickBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
+
+  const average = () => (good - bad) / all
+  const positivePercent = () => (good / all) * 100
 
   return (
     <div>
@@ -47,6 +69,9 @@ const App = () => {
       <Stat stat='good' amount={good}></Stat>
       <Stat stat='neutral' amount={neutral}></Stat>
       <Stat stat='bad' amount={bad}></Stat>
+      <Stat stat='all' amount={all}></Stat>
+      <Stat stat='average' amount={average()}></Stat>
+      <Percent text='positive' amount={positivePercent()}></Percent>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-// exercise 1.6
+// exercise 1.7
 import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
